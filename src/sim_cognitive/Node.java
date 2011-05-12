b4481c1d8f781b1a9170f0cb00c5dc9d518177cd
Angeliki Kordali <aggeliki.kordali@gmail.com>
2011-05-12 17:13:35 +0300

    First commit

---
+++ src/sim_cognitive/Node.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sim_cognitive;
+
+/**
+ *
+ * @author ambassador
+ */
+public class Node {
+    
+    
+}
